c2dd603ad454705ffb78e59490d17ed5520837fa
Lake1059 <vb1059@outlook.com>
2024-04-16 15:31:13 +0800

    2024/4/16-1

--- StardewMUI 2023/功能/管理模组/分类操作.vb
+++ StardewMUI 2023/功能/管理模组/分类操作.vb
@@ -1,6 +1,4 @@
 ﻿Imports System.IO
-Imports System.Xml.Xsl
-Imports Microsoft.VisualBasic.Devices
 Imports Sunny.UI
 
 Public Class 分类操作
@@ -26,16 +24,19 @@ Line1:
 
     Public Shared Sub 转移分类()
         Dim newWindow As New Form With {
-            .Text = "选择目标分类",
+            .Text = "选择目标子库",
             .BackColor = Form1.BackColor,
             .ForeColor = Form1.ForeColor,
             .AutoScroll = True,
             .StartPosition = FormStartPosition.Manual,
-            .Size = New Size(300, 500),
-            .Padding = New Padding(10),
+            .Size = New Size(300 * 界面控制.DPI, 500 * 界面控制.DPI),
+            .MinimumSize = New Size(300 * 界面控制.DPI, 500 * 界面控制.DPI),
+            .Padding = New Padding(10 * 界面控制.DPI),
             .MinimizeBox = False,
             .MaximizeBox = False,
-            .SizeGripStyle = False
+            .SizeGripStyle = False,
+            .ShowInTaskbar = False,
+            .ShowIcon = False
         }
         Dim a As New List(Of String)(共享方法.SearchFolderWithoutSub(管理模组2.检查并返回当前模组数据仓库路径))
         a.Remove(".Download")
@@ -43,7 +44,7 @@ Line1:
         For i = 0 To a.Count - 1
             Dim b1 As New Label With {
                 .AutoSize = False,
-                .Height = 30,
+                .Height = 30 * 界面控制.DPI,
                 .Text = a(i),
                 .TextAlign = ContentAlignment.MiddleLeft,
                 .Dock = DockStyle.Top,
@@ -74,12 +75,31 @@ Line1:
                                      sender.Dispose()
                                  End Sub
             newWindow.Controls.Add(b1)
+            b1.BringToFront()
         Next
         显示模式窗体(newWindow, Form1)
     End Sub
 
     Public Shared Sub 重命名分类()
+        If Form1.ListView1.SelectedItems.Count <> 1 Then Exit Sub
+        Dim a As New 输入对话框("", "重新命名分类：" & Form1.ListView1.SelectedItems(0).Text, Form1.ListView1.SelectedItems(0).Text)
+        a.TranslateButtonText("确定", "取消")
+Line1:
+        Dim s1 As String = a.ShowDialog(Form1)
+        If s1 = "" Then Exit Sub
+        If FileIO.FileSystem.DirectoryExists(管理模组2.检查并返回当前所选子库路径(False) & "\" & s1) Then
+            Dim b As New 多项单选对话框("", {"确定"}, "已经存在目标分类：" & vbCrLf & vbCrLf & s1,, 500)
+            b.ShowDialog(Form1)
+            GoTo Line1
+        Else
+            Dim x As String = Form1.ListView1.SelectedItems(0).Text
+            FileIO.FileSystem.RenameDirectory(管理模组2.检查并返回当前所选子库路径(False) & "\" & x, s1)
+            Form1.ListView1.SelectedItems(0).Text = s1
+            For i = 0 To Form1.ListView2.Items.Count - 1
+                If Form1.ListView2.Items(i).SubItems(3).Text = x Then Form1.ListView2.Items(i).SubItems(3).Text = s1
+            Next
 
+        End If
     End Sub
 
     Public Shared Sub 删除分类()
